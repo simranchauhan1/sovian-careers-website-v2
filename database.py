a38b8f1c69d94730825f48dd403133fa6ec91368
simranchauhan1 <77719573+simranchauhan1@users.noreply.github.com>
2023-05-20 17:26:43 +0000

    updeted database.py

--- database.py
+++ database.py
@@ -3,11 +3,13 @@ import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 my_secret = os.environ['DB_CONNECTION_STRING']
+print(db_connection_string)
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
                          "ssl_ca": "/etc/ssc/cert.pem"
                        }})
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
@@ -15,6 +17,5 @@ def load_jobs_from_db():
     result_dicts = []
     for row in result._allrows():
       result_dicts.append(dict(row._mapping))
-      
+
   return result_dicts
- 
