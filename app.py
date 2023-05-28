3d0d39f6e56349c88d61b95d31e325080d5a0f7c
Simran Chauhan <simranchauhan939@gmail.com>
2023-05-28 23:16:16 +0530

    modified

--- app.py
+++ app.py
@@ -13,13 +13,16 @@ def hello_sovian():
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
+
   return jsonify(jobs)
 
 
-@app.route("/job/<id>")
+
+@app.route("/jobs/<id>")
 def show_job(id):
   job = load_job_from_db(id)
   if not job:
+    
     return "Not Found", 404
   
   return render_template('jobpage.html', job=job)
--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine, text
 import os
+from dotenv import load_dotenv, dotenv_values
 
-db_connection_string = os.environ['DB_CONNECTION_STRING']
-my_secret = os.environ['DB_CONNECTION_STRING']
+load_dotenv()
+
+db_connection_string = os.getenv('DB_CONNECTION_STRING')
+
 print(db_connection_string)
 
 engine = create_engine(db_connection_string,
@@ -23,7 +26,7 @@ def load_jobs_from_db():
 
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),       val=id)
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id = {id} ;"))
     rows = result._allrows()
   if len(rows) == 0:
     return None
@@ -34,13 +37,7 @@ def load_job_from_db(id):
 def add_application_to_db(job_id, data):
   with engine.connect() as conn:
     query = text(
-      "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES(:job_id, :full_name, email, linkedin_url, education, work_experience, resume_url)"
+      f"""INSERT INTO applicaTions (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES({job_id}, "{data['full_name']}", "{data['email']}", "{data['linkedin_url']}", "{data['education']}", "{data['work_experience']}", "{data['resume_url']}")"""
     )
-  conn.execute(query,
-              job_id=job_id,
-              full_name=data['full_name'],
-              email=data['email'],
-              linkedin_url=data['linkedin_url'],
-              education=data['education'],
-              work_experience=data['work_experience'],
-              resume_url=data['resume_url'])
+    conn.execute(query)
+    conn.commit()
